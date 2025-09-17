a51838f259cbf7d2a08e8d4ec14cc306e6eb9579
iancorbett <iancorbett1324@gmail.com>
2025-09-16 22:20:59 -0700

    create shape using console.writeline

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+namespace Shape //namesapce seems to be a way of containing code, for example if i created another shape namespace it would not conflict but there would is there were two classes with the same name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, y'all!");
+
+            //Console.ReadLine();
+
+            
+            Console.WriteLine("  /|");
+            //Console.ReadLine();
+            Console.WriteLine(" / |");
+            Console.WriteLine("/__|");
+        }
+    }
+}
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f7cabc27568c2746f89ec1d2a3717b38c474f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
